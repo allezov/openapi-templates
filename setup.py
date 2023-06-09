eb07d40e6f13ee129d04c25bf71a90a3015a0845
Aleksandr Lezov <al.lezov@yandex.ru>
2023-06-09 14:53:29 +0400

    test3

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ REQUIRES = [
     "urllib3 >= 1.25.3",
     "python-dateutil",
     "structlog",
-    "allure-pytest",
+    "allure-pytest"
 ]
 
 setup(
     name='dm_api_account',
-    version='0.0.1',
-    description="dm_api_account",
+    version='0.0.2',
+    packages=['dm_api_account'],
+    url="https://github.com/allezov/openapi-templates.git",
     license='Apache-2.0 license',
     author="lezov",
     author_email='-',
-    url="https://github.com/allezov/openapi-templates.git",
     install_requires=REQUIRES,
-    packages=['dm_api_account'],
+    description='dm_api_account'
 )
 # setup(
 #     name='dm_api_account_clients',
